df50f61133d2ef0db8dc0b826f937491dcc98f2d
Morley Abuyabor Jr <moravel752@gmail.com>
2022-07-14 15:45:23 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func initializeRouter() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/users", GetUsers).Methods("GET")
+	r.HandleFunc("/user/{id}", GetUser).Methods("GET")
+	r.HandleFunc("/users", CreateUser).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":9000", r))
+}
+
+func main() {
+	InitialMigration()
+	initializeRouter()
+}
